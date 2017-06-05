9238b2fda42420805fc808897c13fbb7f4bcba88
cesarve77 <c@imagenproactiva.com>
2017-06-05 14:45:38 +1000

    roles

---
+++ confirm.js
@@ -0,0 +1,31 @@
+import './confirm.html'
+Template.atConfirm.helpers({
+    //add you helpers here
+});
+
+Template.atConfirm.events({
+    'click .confirm'(e,instance){
+        this.onConfirm()
+        instance.$modal.modal('hide')
+    }
+});
+
+Template.atConfirm.onCreated(function () {
+
+});
+
+Template.atConfirm.onRendered(function () {
+    this.$modal=$(this.firstNode)
+    this.$modal.modal('show')
+    this.$modal.on('hidden.bs.modal',()=>{
+        Blaze.remove(this.view)
+    })
+});
+
+Template.atConfirm.onDestroyed(function () {
+   console.log('Template.atConfirm.onDestroyed')
+});
+
+export const confirm=function({content, onConfirm}){
+    Blaze.renderWithData(Template.atConfirm,{content, onConfirm},document.body)
+}
